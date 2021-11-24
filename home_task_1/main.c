f16a45b24708565f6332b9100f465a22c00279c8
Irina Shepeleva <shepelevairs@gmail.com>
2021-11-25 00:06:15 +0300

    check ci

--- home_task_1/main.c
+++ home_task_1/main.c
@@ -1,20 +1,6 @@
-#include "specialists_db.h"
+#include "specialists_db_lib/specialists_db.h"
 #include <stdio.h>
 
 int main() {
-  // static const size_t max_len = 50;
-  // specialists_t specialists = read_specialists(max_len, stdin);
-  // print_specialists(&specialists);
-  // unsigned int max_age = 0;
-  // scanf("%u", &max_age);
-  // specialists_t filtered_specialists = get_specialists_by_age(&specialists, max_age);
-  // print_specialists(&filtered_specialists);
-  // delete_specialists(&filtered_specialists);
-  static const size_t MAX_LEN = 50;
-  FILE* fin = fopen("../tests/specialists.txt", "r");
-  specialists_t specialists = read_specialists(MAX_LEN, fin);
-  specialists_t filtered_specialists = get_specialists_by_age(&specialists, 26);
-  FILE* fout = fopen("../tests/filtered_specialists.txt", "w");
-  delete_specialists(&specialists);
   return 0;
 }
