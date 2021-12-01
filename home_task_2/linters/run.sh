3b92c2333c38429ef7e839260400674f725a998b
Irina Shepeleva <shepelevairs@gmail.com>
2021-12-01 20:46:23 +0300

    fix workflows

--- home_task_2/linters/run.sh
+++ home_task_2/linters/run.sh
@@ -8,12 +8,12 @@ function print_header() {
 }
 
 print_header "RUN cppcheck"
-cppcheck ./home_task_2/main.cc --enable=all --inconclusive --error-exitcode=1 -I ./home_task_2/matrix_calculator_lib
+cppcheck ./main.cc --enable=all --inconclusive --error-exitcode=1 -I ./matrix_calculator_lib
 
 print_header "RUN clang-tidy"
-clang-tidy ./home_task_2/main.cc -- -I home_task_2/matrix_calculator_lib
+clang-tidy ./main.cc -- -I matrix_calculator_lib
 
 print_header "RUN cpplint.py"
-cd ./home_task_2/ && python3 linters/cpplint/cpplint.py --extensions=c matrix_calculator_lib/* tests/*
+python3 linters/cpplint/cpplint.py --extensions=c matrix_calculator_lib/* tests/*
 
 print_header "SUCCESS"
