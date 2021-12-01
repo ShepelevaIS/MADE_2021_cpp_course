4452ea4c3d8295d7c45041a42ca841efb44b9d48
Irina Shepeleva <shepelevairs@gmail.com>
2021-12-01 19:47:26 +0300

    remove home_task_1

--- home_task_1/tests/test_specialists.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-extern "C" {
-#include "specialists_db.h"
-}
-
-TEST(Specialists_db, TestBasics) {
-  EXPECT_EQ(18, createSpecialist("Irina", 18, "F", "DS", 324000).age);
-}
