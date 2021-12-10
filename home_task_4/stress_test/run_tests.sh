94300db0139165e0efd4a64be7eb4c52d62cc780
Irina Shepeleva <shepelevairs@gmail.com>
2021-12-10 18:12:15 +0300

    fix errors

--- home_task_4/stress_test/run_tests.sh
+++ home_task_4/stress_test/run_tests.sh
@@ -12,7 +12,7 @@ do
   ./build/generate_test/generate_test 5000 $i > $output/input.txt
   ./build/countif_sequential < $output/input.txt > $output/$file1 2>/dev/null
   ./build/countif_parallel < $output/input.txt > $output/$file2 2>/dev/null
-  if cmp -s "$file1" "$file2"; then
+  if cmp -s $output/"$file1" $output/"$file2"; then
       printf 'The file "%s" is the same as "%s"\n' "$file1" "$file2"
   else
       printf 'The file "%s" is different from "%s"\n' "$file1" "$file2"
